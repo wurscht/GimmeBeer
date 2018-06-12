d03940a80cd4675f0f71ed73cb381318021347de
Doyon Mirjam <bdoyom@bbc-be.local>
2018-06-12 13:55:58 +0200

    first design attempt

---
+++ app/src/main/java/ch/bbcag/gimmebeer/MainDetailsActivity.java
@@ -0,0 +1,13 @@
+package ch.bbcag.gimmebeer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_details);
+    }
+}
