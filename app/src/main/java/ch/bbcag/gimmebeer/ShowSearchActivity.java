71238eff057e5a3dcbe60d41f85b0135fd1f61cf
Doyon Mirjam <bdoyom@bbc-be.local>
2018-06-12 14:44:31 +0200

    rename images and continue with design, du wurscht

--- app/src/main/java/ch/bbcag/gimmebeer/ShowSearchActivity.java
+++ app/src/main/java/ch/bbcag/gimmebeer/ShowSearchActivity.java
@@ -3,11 +3,11 @@ package ch.bbcag.gimmebeer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainDetailsActivity extends AppCompatActivity {
+public class ShowSearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_details);
+        setContentView(R.layout.activity_show_search);
     }
 }
