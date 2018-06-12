5bb0abf6fc64675b5a8a8d453a5dcfcf525f9a67
Lehmann Jonas <blehmj@bbc-be.local>
2018-06-12 13:58:01 +0200

    edit PunkApiParser and MainActivity

---
+++ app/src/main/java/ch/bbcag/gimmebeer/ShowAllActivity.java
@@ -0,0 +1,81 @@
+package ch.bbcag.gimmebeer;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+
+import ch.bbcag.gimmebeer.helper.PunkApiParser;
+import ch.bbcag.gimmebeer.model.Beer;
+
+public class ShowAllActivity extends AppCompatActivity {
+    private int beerId;
+    private ProgressBar progressBar;
+    private static final String PUNK_API_URL = "https://api.punkapi.com/v2/beers";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_all);
+        progressBar = findViewById(R.id.progressbar);
+
+    }
+
+    private void loadAllBeer(String url)
+    {
+        final ArrayAdapter<Beer> beerInfoAdapter = new
+                ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1);
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            ArrayList<Beer> beer = PunkApiParser.createBeerFromJsonString(response);
+                            beerInfoAdapter.addAll(beer);
+                            ListView beerInfoList = findViewById(R.id.list_all_beer);
+                            beerInfoList.setAdapter(beerInfoAdapter);
+                            progressBar.setVisibility(View.GONE);
+                        } catch (JSONException e) {
+                            generateAlertDialog();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                generateAlertDialog();
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+    private void generateAlertDialog() {
+        progressBar.setVisibility(View.GONE);
+        AlertDialog.Builder dialogBuilder;
+        dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // Closes this activity
+                finish();
+            }
+        });
+        dialogBuilder.setMessage("Die Badidetails konnten nicht geladen werden. Versuche es später nochmals.").setTitle("Fehler");
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+    }
+}
--- app/src/main/java/ch/bbcag/gimmebeer/helper/PunkApiParser.java
+++ app/src/main/java/ch/bbcag/gimmebeer/helper/PunkApiParser.java
@@ -1,17 +1,31 @@
 package ch.bbcag.gimmebeer.helper;
 
+import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import ch.bbcag.gimmebeer.model.Beer;
 
 public class PunkApiParser {
-    public static Beer[] createBeerFromJsonString(String response) throws JSONException {
+    public static ArrayList<Beer> createBeerFromJsonString(String response) throws JSONException {
         JSONArray beerArray = new JSONArray(response);
+        ArrayList<Beer> allBeers = new ArrayList<>();
         for (int i = 0; i < beerArray.length(); i++) {
             JSONObject beerObject = beerArray.getJSONObject(i);
             Beer beer = new Beer(Integer.parseInt(beerObject.getString("id")), beerObject.getString("name"), beerObject.getDouble("abv"), beerObject.getString("image"), beerObject.getString("tagline"), beerObject.getString("description"));
             JSONArray foodPairingJson = beerObject.getJSONArray("food_pairing");
+            List<String> foodpairing = new ArrayList<String>();
+            for (int j = 0; j < foodPairingJson.length(); j++) {
+                foodpairing.add(foodPairingJson.getJSONObject(j).getString("food_pairing"));
+            }
+            beer.addFoodPairing(foodpairing);
+            allBeers.add(beer);
         }
+        return allBeers;
     }
 }
