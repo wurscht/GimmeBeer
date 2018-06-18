ef4f1d2a0492d13e7be78ede0b6c35daae3b03fe
Doyon Mirjam <bdoyom@bbc-be.local>
2018-06-18 12:09:42 +0200

    working on detail view

--- app/src/main/java/ch/bbcag/gimmebeer/DetailsActivity.java
+++ app/src/main/java/ch/bbcag/gimmebeer/DetailsActivity.java
@@ -1,13 +1,34 @@
 package ch.bbcag.gimmebeer;
 
+import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
     }
+
+    
+    void collapseTextView() {
+        if (txtDescription.getVisibility() == View.GONE) {
+            // it's collapsed - expand it
+            txtDescription.setVisibility(View.VISIBLE);
+        } else {
+            // it's expanded - collapse it
+            txtDescription.setVisibility(View.GONE);
+            txtDescription.setImageResource(R.drawable.ic_expand_more_black_24dp);
+        }
+
+        ObjectAnimator animation = ObjectAnimator.ofInt(txtDescription, "maxLines", txtDescription.getMaxLines());
+        animation.setDuration(200).start();
+    }
+
 }
